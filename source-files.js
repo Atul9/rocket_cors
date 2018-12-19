a9b6f9a6d374bf704c0026cce9c1035b1b6b245d
Travis CI User <travis@example.org>
2018-12-19 00:57:28 +0000

    Documentation for lawliet89/rocket_cors@9f87c73

---
+++ source-files.js
@@ -0,0 +1,8 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
+sourcesIndex['rocket_cors'] = {"name":"","dirs":[],"files":["fairing.rs","headers.rs","lib.rs"]};
